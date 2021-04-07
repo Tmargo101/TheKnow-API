b3122bf0ead66424f6dd7914e750e03142f9aaa0
Thomas Margosian <tmargo101@gmail.com>
2021-04-07 15:35:06 +0000

    - When a new place is added, it will add it’s ID to the collectionID which is passed to it.

--- server/controllers/Place.js
+++ server/controllers/Place.js
@@ -1,12 +1,13 @@
 const models = require('../models');
 
-const { Place } = models;
+const { Place, Collection } = models;
 
 const addPlace = (request, response) => {
   if (!request.body.name || !request.body.mapLink || !request.body.addedBy) {
     response.send('Not all params');
     return;
   }
+  
   const newPlace = new Place.PlaceModel({
     name: request.body.name,
     addedBy: request.body.addedBy,
@@ -14,12 +15,25 @@ const addPlace = (request, response) => {
       maps: request.body.mapLink,
     },
   });
-
-  newPlace.save().then(() => {
+  
+  // Added async so we can await results of FindByIdAndUpdate  
+  newPlace.save().then( async () => {
     Place.PlaceModel.toAPI(newPlace);
+    
+    // Now that we have the newPlace ID, add it to the Collection
+    const parentCollection = await Collection.CollectionModel.findByIdAndUpdate(
+      request.body.collectionID,
+      {
+        $push: {
+          places: newPlace._id
+        }
+      }
+    ).exec();
+    
     return response.json({
       success: 'saved',
-      object: newPlace,
+      placeID: newPlace._id,
+      collectionID: parentCollection._id
     });
   });
 };
--- server/router.js
+++ server/router.js
@@ -5,8 +5,8 @@ const router = (app) => {
   app.get('/logout', controllers.Account.logout);
   app.post('/login', controllers.Account.login);
   app.post('/signup', controllers.Account.signup);
-  app.post('/places', controllers.Place.addPlace);
-  app.get('/places/:id', controllers.Place.getPlace);
+  app.post('/place', controllers.Place.addPlace);
+  app.get('/place/:id', controllers.Place.getPlace);
   app.post('/collection', controllers.Collection.addCollection);
 };
 
