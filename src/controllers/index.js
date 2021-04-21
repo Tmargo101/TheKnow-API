822915f818c05a3313aec1fd08b8a5b6a6fa8ba0
Thomas Margosian <tmargo101@gmail.com>
2021-04-21 18:05:45 -0400

    Added Babel & updated some scripts in package.json to use it.

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,4 @@
-module.exports.Home = require('./Home.js');
+module.exports.Home = require('./home.js');
 module.exports.Place = require('./Place.js');
 module.exports.Account = require('./Account.js');
 module.exports.Collection = require('./Collection.js');
